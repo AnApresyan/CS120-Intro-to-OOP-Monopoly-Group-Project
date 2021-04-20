f9c354f9b3d753eb34ce6209608b0eb0f7ae83a2
Anahit Apresyan <aapresya@c1r10s3.42yerevan.am>
2021-04-20 15:18:21 +0400

    oof

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,3 +1,4 @@
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,26 +13,31 @@ public class MainWindow extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1500, 1500);
         this.setVisible(true);
-
-        JPanel mainMenu = new JPanel();
-        JLabel label = new JLabel();
-
         ImageIcon image = new ImageIcon("./images/logo2.png");
-
         
-        label.setText("Welcome to Alexander's and Anahit's Monopoly");
-        /*JButton start = new JButton("START");
+        JPanel mainMenu = new JPanel();
+        mainMenu.setLayout(new GridLayout(2, 1));
+
+        JLabel label = new JLabel("Welcome to Alexander's and Anahit's Monopoly");
+
+        JButton start = new JButton("START");
+        start.setVerticalAlignment(JButton.BOTTOM);
         start.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
                 start.setVisible(false);
                 
             }
-        });*/
+        });
+
+        
         label.setIcon(image);
+        label.add(start);
         label.setHorizontalTextPosition(JLabel.CENTER);
         label.setVerticalTextPosition(JLabel.BOTTOM);
+        
         mainMenu.add(label);
+        mainMenu.add(start);
         this.add(mainMenu);
         
         
